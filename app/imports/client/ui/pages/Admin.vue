28f43e8da79cc99325ab2c88c4b0cf4747dbd44a
yolego20 <yolego20@gmail.com>
2024-10-25 19:28:12 -0400

    Revert "Added IconAdmin to admin panel"
    This reverts commit bda72f5342f706c18ecbf82793401f447d15ed77.

--- app/imports/client/ui/pages/Admin.vue
+++ app/imports/client/ui/pages/Admin.vue
@@ -47,12 +47,6 @@
             </v-alert>
           </v-card-text>
         </v-card>
-        <v-card class="mt-4">
-          <v-card-title>Icon Administration</v-card-title>
-          <v-card-text>
-            <icon-admin />
-          </v-card-text>
-        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -62,12 +56,8 @@
 import getVersion from '/imports/migrations/methods/getVersion.js';
 import migrateTo from '/imports/migrations/methods/migrateTo.js';
 import SCHEMA_VERSION from '/imports/constants/SCHEMA_VERSION.js';
-import IconAdmin from '/imports/client/ui/icons/IconAdmin.vue';
 
 export default {
-  components: {
-    IconAdmin,
-  },
   data(){return {
     loadingVersion: false,
     versions: {},
