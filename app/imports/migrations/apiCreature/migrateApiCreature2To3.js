865c7a5ca2ab10dbb56ba321511e39a0bf104f48
ThaumRystra <thaum@dicecloud.com>
2024-11-03 14:35:25 +0200

    Ensure creature properties are sorted before being migrated from an archive

--- app/imports/migrations/apiCreature/migrateApiCreature2To3.js
+++ app/imports/migrations/apiCreature/migrateApiCreature2To3.js
@@ -1,12 +1,14 @@
 import migrateProperty2To3 from '/imports/migrations/archive/properties/migrateProperty2To3';
 
 export default function migrateApiCreature2To3(creature) {
-  creature.creatureProperties = creature.creatureProperties.map(prop => {
-    try {
-      migrateProperty2To3(prop);
-    } catch (e) {
-      console.warn('Property migration 2 -> 3 failed: ', { propId: prop._id, error: e.message || e.reason || e.toString() });
-    }
-    return prop;
-  });
+  creature.creatureProperties = creature.creatureProperties
+    .sort((a, b) => a.order - b.order)
+    .map(prop => {
+      try {
+        migrateProperty2To3(prop);
+      } catch (e) {
+        console.warn('Property migration 2 -> 3 failed: ', { propId: prop._id, error: e.message || e.reason || e.toString() });
+      }
+      return prop;
+    });
 }
